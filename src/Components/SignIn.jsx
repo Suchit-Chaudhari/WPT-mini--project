e6443932436a1594ff5470372180ddd4e149f13a
Suchit <suchit2409@gmail.com>
2024-05-16 15:22:22 +0530

    added login page with password validation

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+
+import '../css/SignIn.css';
+import { Button, TextField } from '@mui/material';
 
 const SignIn = () => {
-  return (
-    <div>
-      Sign In
-    </div>
-  )
-}
+    const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState('');
+    const [passwordError, setPasswordError] = useState('');
 
-export default SignIn
+    const handleUserName = (e) => {
+        setUserName(e.target.value);
+        console.log(userName);
+    }
+
+    
+    const validatePassword = (e) => {
+        const value = e.target.value;
+        setPassword(value);
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[A-Za-z\d!@#$%^&*()_+]{8,}$/;
+        
+        if (!passwordRegex.test(value)) {
+        setPasswordError('Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.');
+        } else {
+        setPasswordError('');
+        }
+        console.log(password)
+    };
+    return (
+        <div className="background">
+            <div className='sign-in-container'>
+            <h2> Sign In</h2>
+            <TextField 
+                fullWidth 
+                id="userName"
+                label="Enter Your Username" 
+                variant="standard" 
+                className='my-3' 
+                required 
+                onChange={handleUserName}/>
+            <TextField 
+                fullWidth 
+                id="password" 
+                label="Enter Your Password" 
+                variant="standard" 
+                className='my-3' 
+                type='password' 
+                required 
+                onChange={validatePassword}
+                error={!!passwordError} 
+                helperText={passwordError}/>
+            <Button variant="contained" className='my-3' required>Sign In</Button>
+        </div>
+        </div>
+        
+    );
+};
+
+export default SignIn;
